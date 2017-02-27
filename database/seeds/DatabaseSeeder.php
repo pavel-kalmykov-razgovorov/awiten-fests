bd6e9d752b983591488fc0aeb1c6bf96d742420d
Pepe <perez_sifre@hotmail.com>
2017-02-27 20:31:44 +0100

    Datos en tablas principales

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ArtistsTableSeeder::class);
+        $this->call(FestivalsTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
     }
 }
