3b105e93d9b2307c940dcbbb991bc7573bae5fd5
Pepe <perez_sifre@hotmail.com>
2017-02-28 18:23:36 +0100

    Seeders tablas relaciones

---
+++ database/seeds/Artist_FestivalsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Artist_FestivalsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
