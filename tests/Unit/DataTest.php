639e528c20cf35f316a8cdf39941432edcd11942
Pavel Razgovorov <paveltrufi@hotmail.com>
2017-03-03 14:16:46 +0100

    Creados tests de datos para las clases principales

--- tests/Unit/DataTest.php
+++ tests/Unit/DataTest.php
@@ -1,144 +1,127 @@
 <?php
-/*
+
 namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-use App\Sponsor;
-use App\Team;
-use App\Player;
+use App\Artist;
+use App\Festival;
+use App\Genre;
 
 class DataTest extends TestCase
 {
     /**
-     * Checks the number and names of the sponsors
-     *
+     * Checks that some table has the specified entries
+     * 
+     * @param string $table the name of the table
+     * @param callable $ass_arr_col a function that returns an associative array corresponding to the table's cols
+     * @param $entries an array of arrays with all the entries that have to be checked.
+     *        Every array must have the proper structure in order to fit with the $ass_arr_col's parameters!
      * @return void
-     
-    public function testSponsorsData()
-    {
-        $count = Sponsor::all()->count();
-        $this->assertEquals($count, 5);
-
-        $this->assertDatabaseHas('sponsors', ['name' => 'Samsung']);
-        $this->assertDatabaseHas('sponsors', ['name' => 'Toshiba']);
-        $this->assertDatabaseHas('sponsors', ['name' => 'Asus']);
-        $this->assertDatabaseHas('sponsors', ['name' => 'Intel']);
-        $this->assertDatabaseHas('sponsors', ['name' => 'AMD']);
+     */
+    public function checkTableEntries ($table, $ass_arr_col, $entries) {
+        foreach ($entries as $entry) {
+            $this->assertDatabaseHas($table, call_user_func_array($ass_arr_col, $entry));
+        }
     }
 
     /**
-     * Checks the number and names of the teams
-     *
+     * Checks festivals' seeds
+     * 
      * @return void
-     
-    public function testTeamsData()
-    {
-        $count = Team::all()->count();
-        $this->assertEquals($count, 7);
+     */
+    public function testFestivalsData() {
+        $get_festival_array = function ($name, $address, $date, $province) {
+            return [
+                'name' => $name,
+                'address' => $address,
+                'date' => $date,
+                'province' => $province
+            ];
+        };
 
-        $this->assertDatabaseHas('teams', ['name' => 'Screaming Nachos']);
-        $this->assertDatabaseHas('teams', ['name' => 'Elemonators']);
-        $this->assertDatabaseHas('teams', ['name' => 'E = MC Hammer']);
-        $this->assertDatabaseHas('teams', ['name' => 'Chili Peppers']);
-        $this->assertDatabaseHas('teams', ['name' => 'Low Expectations']);
-        $this->assertDatabaseHas('teams', ['name' => 'Rescheduled']);
-        $this->assertDatabaseHas('teams', ['name' => 'Get Your Kicks']);
+        $this->assertEquals(7, Festival::all()->count());
+        $this->checkTableEntries(
+            'festivals',
+            $get_festival_array,
+            [
+                ["Medusa", "Calle Doctor Marañon Nº21", "17/08/2017", "Valencia"],
+                ["Arenal", "Avd Castellon Nº11", "27/08/2017", "Castellon"],
+                ["Dreambeach", "Avd Jaume I Nº82", "17/07/2017", "Almeria"],
+                ["Barcelona Beach Festival", "Calle Barceloneta", "11/07/2017", "Barcelona"],
+                ["A summer story", "Calle Falsa 123", "14/07/2017", "Madrid"],
+                ["Tomorrowland", "Calle Bredlak Nº12", "17/08/2017", "Boom"],
+                ["Ultra Music Festival", "Green Street", "23/06/2017", "Miami"]
+            ]
+        );
     }
 
     /**
-     * Checks the number and data of the players
-     *
+     * Checks artists' seeds
+     * 
      * @return void
-     
-    public function testPlayersData()
-    {
-        $count = Player::all()->count();
-        $this->assertEquals($count, 9);
+     */
+    public function testArtistsData() {
+        $get_artist_array = function ($name, $soundcloud, $website, $country) {
+            return [
+                'name' => $name,
+                'soundcloud' => $soundcloud,
+                'website' => $website,
+                'country' => $country
+            ];
+        };
 
-        $this->assertDatabaseHas('players', ['name' => 'John Doe', 'age' => 17]);
-        $this->assertDatabaseHas('players', ['name' => 'Jim Baker', 'age' => 16]);
-        $this->assertDatabaseHas('players', ['name' => 'Nick Alias', 'age' => 21]);
-        $this->assertDatabaseHas('players', ['name' => 'Steve Bacon', 'age' => 17]);
-        $this->assertDatabaseHas('players', ['name' => 'Mike Connor', 'age' => 18]);
-        $this->assertDatabaseHas('players', ['name' => 'Tim Eater', 'age' => 17]);
-        $this->assertDatabaseHas('players', ['name' => 'Billy Sheen', 'age' => 17]);
-        $this->assertDatabaseHas('players', ['name' => 'Sam Uncle', 'age' => 17]);
-        $this->assertDatabaseHas('players', ['name' => 'Rick Allister', 'age' => 19]);
+        $this->assertEquals(19, Artist::all()->count());
+        $this->checkTableEntries(
+            'artists',
+            $get_artist_array,
+            [
+                ["Joris Voorn", "https://soundcloud.com/joris-voorn", "http://www.jorisvoorn.com/", "Holanda"],
+                ["Paco Osuna", "https://soundcloud.com/paco-osuna", "http://www.pacoosuna.com/", "España"],
+                ["Richie Hawtin", "https://soundcloud.com/richiehawtin", "http://plastikman.com/", "Canadá"],
+                ["Carl Cox", "https://soundcloud.com/carl-cox", "http://www.carlcox.com/", "Estados Unidos"],
+                ["Adam Beyer", "https://soundcloud.com/adambeyer", "http://www.drumcode.se/adambeyer.html", "Suecia"],
+                ["Marco Carola", "https://soundcloud.com/marcocarola", "https://www.residentadvisor.net/dj/marcocarola", "Italia"],
+                ["Joseph Capriati", "https://soundcloud.com/joseph-capriati", "http://www.josephcapriati.com/", "Italia"],
+                ["Loco Dice", "https://soundcloud.com/locodiceofc", "http://locodiceofc.tumblr.com/", "Alemania"],
+                ["Paul Ritch", "https://soundcloud.com/paulritch", "http://www.paulritch.com/", "Francia"],
+                ["Pan-Pot", "https://soundcloud.com/pan-pot", "http://pan-pot.net/", "Alemania"],
+                ["Armin van Buuren", "https://soundcloud.com/arminvanbuuren", "http://www.arminvanbuuren.com/", "Holanda"],
+                ["Paul van Dyk", "https://soundcloud.com/paulvandykofficial", "http://www.paulvandyk.com/", "Alemania"],
+                ["http://www.sandervandoorn.com/", "Sander van Doorn", "17", "Holanda"],
+                ["Don Diablo", "https://soundcloud.com/dondiablo", "http://www.dondiablo.com/", "Holanda"],
+                ["Hardwell", "https://soundcloud.com/hardwell", "https://www.djhardwell.com/", "Holanda"],
+                ["Galantis", "https://soundcloud.com/wearegalantis", "http://www.wearegalantis.com/", "Suecia"],
+                ["Coone", "https://soundcloud.com/coone", "http://www.djcoone.com/", "Bélgica"],
+                ["Zatox", "https://soundcloud.com/djzatox", "http://www.djzatox.com/", "Italia"],
+                ["Brennan Heart", "https://soundcloud.com/brennanheart", "http://www.brennanheart.com/", "Holanda"]
+            ]
+        );
     }
 
     /**
-     * Checks the teams associated to the sponsors
-     *
+     * Checks genres' seeds
+     * 
      * @return void
-     
-    public function testTeamsBySponsor()
-    {
-        $sponsor = Sponsor::where('name', 'Samsung')->first();
-        $this->assertEquals($sponsor->teams->count(), 2);
-        $this->assertTrue($sponsor->teams->contains('name', 'Screaming Nachos'));
-        $this->assertTrue($sponsor->teams->contains('name', 'Elemonators'));
+     */
+    public function testGenresData() {
+        $get_genre_array = function ($genre) {
+            return ['genre' => $genre];
+        };
 
-        $sponsor = Sponsor::where('name', 'Toshiba')->first();
-        $this->assertEquals($sponsor->teams->count(), 2);
-        $this->assertTrue($sponsor->teams->contains('name', 'E = MC Hammer'));
-        $this->assertTrue($sponsor->teams->contains('name', 'Chili Peppers'));
-
-        $sponsor = Sponsor::where('name', 'Asus')->first();
-        $this->assertEquals($sponsor->teams->count(), 1);
-        $this->assertTrue($sponsor->teams->contains('name', 'Low Expectations'));
-
-        $sponsor = Sponsor::where('name', 'Intel')->first();
-        $this->assertEquals($sponsor->teams->count(), 1);
-        $this->assertTrue($sponsor->teams->contains('name', 'Rescheduled'));
-
-        $sponsor = Sponsor::where('name', 'AMD')->first();
-        $this->assertEquals($sponsor->teams->count(), 1);
-        $this->assertTrue($sponsor->teams->contains('name', 'Get Your Kicks'));
-    }
-
-    /**
-     * Checks the players assigned to each team
-     *
-     * @return void
-     
-    public function testPlayersByTeam()
-    {
-        $team = Team::where('name', 'Screaming Nachos')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'John Doe'));
-        $this->assertTrue($team->players->contains('name', 'Jim Baker'));
-
-        $team = Team::where('name', 'Elemonators')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'Jim Baker'));
-        $this->assertTrue($team->players->contains('name', 'Sam Uncle'));
-
-        $team = Team::where('name', 'E = MC Hammer')->first();
-        $this->assertEquals($team->players->count(), 1);
-        $this->assertTrue($team->players->contains('name', 'John Doe'));
-
-        $team = Team::where('name', 'Chili Peppers')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'Nick Alias'));
-        $this->assertTrue($team->players->contains('name', 'Sam Uncle'));
-
-        $team = Team::where('name', 'Low Expectations')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'Steve Bacon'));
-        $this->assertTrue($team->players->contains('name', 'Billy Sheen'));
-
-        $team = Team::where('name', 'Rescheduled')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'Mike Connor'));
-        $this->assertTrue($team->players->contains('name', 'Tim Eater'));
-
-        $team = Team::where('name', 'Get Your Kicks')->first();
-        $this->assertEquals($team->players->count(), 2);
-        $this->assertTrue($team->players->contains('name', 'Tim Eater'));
-        $this->assertTrue($team->players->contains('name', 'Rick Allister'));
+        $this->assertEquals(5, Genre::all()->count());
+        $this->checkTableEntries(
+            'genres',
+            $get_genre_array,
+            [
+                ["Techno"],
+                ["EDM"],
+                ["House"],
+                ["Trance"],
+                ["Hardstyle"]
+            ]
+        );
     }
 }
-*/
