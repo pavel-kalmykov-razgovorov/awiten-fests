d380bc1529ab50cf1dc6d7c82942cf9ba1312716
Pepe <perez_sifre@hotmail.com>
2017-03-27 12:17:35 +0200

    arreglamos error con rutas

--- resources/views/artist/details.blade.php
+++ resources/views/artist/details.blade.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    {!! HTML::style('css/style.css') !!}
-    <title>Awiten Fests</title>
-</head>
+@extends('welcome')
+@section('menu')
 
-<body>
 <h1>Detalles Artista</h1>
 <ul>
+    <div class="item active" style="background-image: url(images/festival-fondo2.jpg)">
+        <!--<img class="img-responsive" src="{!! HTML::style('css/diablo.jpg') !!}" alt="Chania" width="460" height="345"> -->
     <li>Nombre: {{$artist->name}}</li>
     <li>SoundCloud: <a href="{{$artist->soundcloud}}">{{$artist->soundcloud}}</a></li>
     <li>Sitio Web: <a href="{{$artist->website}}">{{$artist->website}}</a></li>
     <li>País: {{$artist->country}}</li>
+    </div>
     <li>
         Festivales:
         <ul>
@@ -34,5 +29,5 @@
     <a href="/artists">Artistas</a>
     <a href="/">Inicio</a>
 </p>
-</body>
-</html>
+
+@endsection
