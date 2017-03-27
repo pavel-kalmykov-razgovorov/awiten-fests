614b78d15ec18f86e84575ca1692b8d42d3ae53d
Arnau Monllor <arnau1020@gmail.com>
2017-03-27 11:59:25 +0200

    Seeders de fotos y noticias e imágenes de festivales y artistas

--- database/seeds/PhotosTableSeeder.php
+++ database/seeds/PhotosTableSeeder.php
@@ -25,7 +25,42 @@ class PhotosTableSeeder extends Seeder
                 DB::table('photos')->delete();
                 DB::table('photos')->insert(
                     [
-                        ['path' => '/images', 'name' => 'foto1', 'festival_id' => $medusa]
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/aquasella', 'name' => 'aquasella1.jpg', 'festival_id' => $aquasella],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/aquasella', 'name' => 'aquasella2.jpg', 'festival_id' => $aquasella],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/aquasella', 'name' => 'aquasella3.jpg', 'festival_id' => $aquasella],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/arenal', 'name' => 'arenal-sound1.jpg', 'festival_id' => $arenal],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/arenal', 'name' => 'arenal-sound2.jpg', 'festival_id' => $arenal],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/asummerstory', 'name' => 'a-summer-story1.jpg', 'festival_id' => $sstory],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/asummerstory', 'name' => 'a-summer-story2.jpg', 'festival_id' => $sstory],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/awakenings', 'name' => 'awakenings1.jpg', 'festival_id' => $awakenings],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/awakenings', 'name' => 'awakenings2.jpg', 'festival_id' => $awakenings],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/awakenings', 'name' => 'awakenings3.jpg', 'festival_id' => $awakenings],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/awakenings', 'name' => 'awakenings4.jpg', 'festival_id' => $awakenings],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/awakenings', 'name' => 'awakenings5.jpg', 'festival_id' => $awakenings],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/dreambeach', 'name' => 'dreambeach1.jpg', 'festival_id' => $dreambeach],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/dreambeach', 'name' => 'dreambeach2.jpg', 'festival_id' => $dreambeach],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/jaco', 'name' => 'creamfields1.jpg', 'festival_id' => $jaco],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/jaco', 'name' => 'creamfields2.jpg', 'festival_id' => $jaco],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/jaco', 'name' => 'creamfields3.jpg', 'festival_id' => $jaco],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/jaco', 'name' => 'creamfields4.jpg', 'festival_id' => $jaco],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/medusa', 'name' => 'medusa1.jpg', 'festival_id' => $medusa],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/medusa', 'name' => 'medusa2.jpg', 'festival_id' => $medusa],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/medusa', 'name' => 'medusa3.jpg', 'festival_id' => $medusa],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/medusa', 'name' => 'medusa4.jpg', 'festival_id' => $medusa],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/medusa', 'name' => 'medusa-raul-barcia.jpg', 'festival_id' => $medusa],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland1.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland2.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland3.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland4.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland5.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/tomorrowland', 'name' => 'tomorrowland6.jpg', 'festival_id' => $tomorrow],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/umf', 'name' => 'UMF1.jpg', 'festival_id' => $umf],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/umf', 'name' => 'UMF2.jpg', 'festival_id' => $umf],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/umf', 'name' => 'UMF-3.jpg', 'festival_id' => $umf],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/umf', 'name' => 'umf4.jpg', 'festival_id' => $umf],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/wan', 'name' => 'wan1.jpg', 'festival_id' => $wan],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/wan', 'name' => 'wan2.jpeg', 'festival_id' => $wan],
+                        ['path' => '/storage/app/public/awiten-fests-data/festivales/wan', 'name' => 'wan3.png', 'festival_id' => $wan]
 
                     ]
                 );
