57db6b107971be873ef977c2eeb374398396526d
Pavel Razgovorov <paveltrufi@hotmail.com>
2017-04-03 11:37:40 +0200

    Cambiadas vistas y duplicadas las de festival (con y sin plantilla)

--- resources/views/festival/details.blade.php
+++ resources/views/festival/details.blade.php
@@ -1,30 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Awiten Fests</title>
-</head>
-
-<body>
-<h1>{{$festival->name}}</h1>
-<ul>
-    <li>Localizacion: {{$festival->location}}</li>
-    <li>Fecha: {{$festival->date}}</li>
-    <li>Provincia: {{$festival->province}}</li>
-    <li>
-        Artistas:
-        <ul>
-            @forelse($festival->artists as $artist)
-                <li><a href="/artist/{{$artist->permalink}}">{{$artist->name}}</a></li>
-            @empty
-                Ninguno
-            @endforelse
-        </ul>
-    </li>
-</ul>
-<a href="/festivals">Festivales</a>
-<a href="/">Inicio</a>
-</body>
-</html>
+<h1>Detalles Festival</h1>
+@if(session('created'))
+    <h3>El festival se ha creado correctamente</h3>
+@endif
+@if(session('updated'))
+    <h3>El festival se ha modificado correctamente</h3>
+@endif
+@if(isset($festival))
+    <ul>
+        <li>Nombre: {{$festival->name}}</li>
+        <li>Ruta logo: {{$festival->pathLogo}}</li>
+        <li>Ruta cartel: {{$festival->pathCartel}}</li>
+        <li>Localizacion: {{$festival->location}}</li>
+        <li>Provincia: {{$festival->province}}</li>
+        <li>Fecha: {{$festival->date}}</li>
+        <li>
+            Artistas:
+            <ul>
+                @forelse($festival->artists as $artist)
+                    <li><a href="/artist/{{$artist->permalink}}">{{$artist->name}}</a></li>
+                @empty
+                    Ninguno
+                @endforelse
+            </ul>
+        </li>
+    </ul>
+    <p>
+        <input type="button" onclick="location.href='{{action('FestivalController@Delete', $permalink)}}';"
+               value="Borrar"/>
+        <input type="button" onclick="location.href='{{action('FestivalController@Edit', $permalink)}}';"
+               value="Editar"/>
+    </p>
+@else
+    <h3>El festival {{str_replace('-', ' ', title_case($permalink))}} no existe. Probablemente haya sido borrado de la
+        base de datos</h3>
+@endif
