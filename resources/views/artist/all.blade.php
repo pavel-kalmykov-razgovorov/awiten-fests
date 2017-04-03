70eb5552575280b08244440f3e21816079bbc7b6
Pepe <perez_sifre@hotmail.com>
2017-04-03 17:30:04 +0200

    vistas

--- resources/views/artist/all.blade.php
+++ resources/views/artist/all.blade.php
@@ -1,21 +1,21 @@
 @extends('welcome')
 @section('menu')
 <div class="arreglar-margen">
-<h1>Artistas</h1>
+
 @if(session('deleted'))
     <h3>El artista se ha borrado correctamente</h3>
 @endif
+<h1 style="background-color:rgb(90,90,90);color:white; text-align: center">Artistas</h1>   
 <ul>
     @forelse($artists as $artist)
-         <div>
-                <img class = "lista-artistas-prinicpal" src="{{ asset('images/artistas/' . trim($artist->permalink) . '/' . 'profile.jpg') }}">
-                <div>
-                <a href="/artist/{{$artist->permalink}}"> {{$artist->name}}</a>
-                </div>
-            </div>
+        <a href="/artist/{{$artist->permalink}}"> 
+            <img class = "lista-artistas-prinicpal" src="{{ asset('images/artistas/' . trim($artist->permalink) . '/' . 'profile.jpg') }}">
+        </a>
     @empty
         <h2>No hay artistas en la BD</h2>
+        {{ $artist->links() }}
     @endforelse
+    
 </ul>
 <p>
     <input type="button" onclick="location.href='{{action('ArtistController@New')}}';" value="Nuevo artista"/>
