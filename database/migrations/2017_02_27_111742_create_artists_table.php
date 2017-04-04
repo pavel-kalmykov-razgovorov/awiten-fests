ab2cf0209c91872d6c291e3ce6323b9c5438224f
Pavel Razgovorov <paveltrufi@hotmail.com>
2017-04-04 17:11:47 +0200

    Actualizada tabla de artista para guardar rutas de la foto de perfil y la imagen de cabecera

--- database/migrations/2017_02_27_111742_create_artists_table.php
+++ database/migrations/2017_02_27_111742_create_artists_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateArtistsTable extends Migration
 {
@@ -20,6 +20,8 @@ class CreateArtistsTable extends Migration
             $table->string('soundcloud')->nullable();
             $table->string('website')->nullable();
             $table->string('country')->nullable();
+            $table->string('pathProfile')->nullable();
+            $table->string('pathHeader')->nullable();
             $table->string('permalink')->unique();
         });
     }
