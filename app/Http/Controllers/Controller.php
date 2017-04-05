a5b7744a369f427985788f9e4580e3ce7a3404c9
Pavel Razgovorov <paveltrufi@hotmail.com>
2017-04-05 10:03:07 +0200

    Arreglados un par de pequeños detalles del último commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,30 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
-
-/*
- @if (count($festival->photos) != 0)
-                <div class="item active">
-                    <img src="{{$festival->photos->get(0)}}" alt="Chania">
-                </div>
-            @endif
-            @for ($i = 1; $i < ($festival->photos); $i++)
-                <div class="item">
-                    <img src="{{$festival->photos->get($i)}}" alt="Chania">
-                </div>
-$festival = App\Festival::where('id',1)->get();
-                 
-            @endfor*/
-
            
