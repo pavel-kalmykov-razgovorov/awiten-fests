b180a076174e42d079087aa18e2d77d3df93be1f
Pepe <perez_sifre@hotmail.com>
2017-04-05 10:33:43 +0200

    arreglando imagenes

--- resources/views/festival/details.blade.php
+++ resources/views/festival/details.blade.php
@@ -1,9 +1,6 @@
 @extends('welcome')
-
-
 @section('mainContent')
 <div class="arreglar-margen">
-
 <h1 class=".text-center">Detalles Festival</h1>
 @if(session('created'))
     <h3>El festival se ha creado correctamente</h3>
@@ -18,7 +15,6 @@
 </ul>
 <div class="container">
     	<div class="row">
-
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
@@ -26,21 +22,19 @@
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
   </ol>
-
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
               @if ($festival->photos()->count() != 0)
                 <div class="item active">
-                    <img src="{{$festival->photos->get(0)->path}}" alt="400" width="1000">
+                    <img src="{{$festival->photos->get(0)->path}}">
                 </div>
             @endif
             @for ($i = 1; $i < $festival->photos()->count(); $i++)
                 <div class="item">
-                    <img src="{{$festival->photos->get($i)->path}}" alt="400" width="1000">
+                    <img src="{{$festival->photos->get($i)->path}}">
                 </div>
                 @endfor
   </div>
-
   <!-- Left and right controls -->
   <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -53,11 +47,9 @@
 </div>
 </div>
 </div>
-
     <!--li>Localizacion: {{$festival->location}}</li>
     <li>Provincia: {{$festival->province}}</li>
     <li>Fecha: {{\Carbon\Carbon::parse($festival->date)->format('d/m/Y')}}</li-->
-
     <div class="container">
     	<div class="row">
 			<div class="[ col-xs-2 col-sm-offset-2 col-sm-2 ]">
@@ -108,64 +100,70 @@
 @endforelse
 </div>
 @endsection
-
 <style type="text/css">
 	@media (min-width: 768px) {
- .event-list {
-		list-style: none;
-		font-family: 'Lato', sans-serif;
-     margin: 0;
-     padding: 0;
-	}
-	.event-list > li {
-		background-color: rgb(255, 255, 255);
-        box-shadow: 0 0 5px rgba(51, 51, 51, 0.7);
-        padding: 0;
-        margin: 0 0 20px;
-	}
-	.event-list > li > time {
-		display: inline-block;
-		width: 100%;
-		color: rgb(255, 255, 255);
-		background-color: rgb(197, 44, 50);
-		padding: 5px;
-		text-align: center;
-		text-transform: uppercase;
-	}
-	.event-list > li:nth-child(even) > time {
-		background-color: rgb(165, 82, 167);
-	}
-	.event-list > li > time > span {
-		display: none;
-	}
-	.event-list > li > time > .day {
-		display: block;
-		font-size: 56pt;
-		font-weight: 100;
-		line-height: 1;
-	}
-	.event-list > li time > .month {
-		display: block;
-		font-size: 24pt;
-		font-weight: 900;
-		line-height: 1;
-	}
-	.event-list > li > img {
-		width: 100%;
-	}
-	.event-list > li > .info {
-		padding-top: 5px;
-		text-align: center;
-	}
-	.event-list > li > .info > .title {
-		font-size: 17pt;
-		font-weight: 700;
-        margin: 0;
-	}
-	.event-list > li > .info > .desc {
-		font-size: 13pt;
-		font-weight: 300;
-        margin: 0;
-	}
+		.carousel-inner {
+			height: 500px;
+		}
+		.carousel-inner img {
+			height: 100%;
+			width: 100%;
+		}
+		.event-list {
+			list-style: none;
+			font-family: 'Lato', sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+		.event-list > li {
+			background-color: rgb(255, 255, 255);
+			box-shadow: 0 0 5px rgba(51, 51, 51, 0.7);
+			padding: 0;
+			margin: 0 0 20px;
+		}
+		.event-list > li > time {
+			display: inline-block;
+			width: 100%;
+			color: rgb(255, 255, 255);
+			background-color: rgb(197, 44, 50);
+			padding: 5px;
+			text-align: center;
+			text-transform: uppercase;
+		}
+		.event-list > li:nth-child(even) > time {
+			background-color: rgb(165, 82, 167);
+		}
+		.event-list > li > time > span {
+			display: none;
+		}
+		.event-list > li > time > .day {
+			display: block;
+			font-size: 56pt;
+			font-weight: 100;
+			line-height: 1;
+		}
+		.event-list > li time > .month {
+			display: block;
+			font-size: 24pt;
+			font-weight: 900;
+			line-height: 1;
+		}
+		.event-list > li > img {
+			width: 100%;
+		}
+		.event-list > li > .info {
+			padding-top: 5px;
+			text-align: center;
+		}
+		.event-list > li > .info > .title {
+			font-size: 17pt;
+			font-weight: 700;
+			margin: 0;
+		}
+		.event-list > li > .info > .desc {
+			font-size: 13pt;
+			font-weight: 300;
+			margin: 0;
+		}
     }
 </style>
