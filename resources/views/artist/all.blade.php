6ac957703e4c904fa8890fd7c993d9a026d310c3
Arnau Monllor <arnau1020@gmail.com>
2017-04-05 23:12:58 +0200

    Página de artistas mejorada

--- resources/views/artist/all.blade.php
+++ resources/views/artist/all.blade.php
@@ -1,31 +1,18 @@
 @extends('welcome')
 @section('mainContent')
-
+<div class="arreglar-margen">
 @if(session('deleted'))
     <h3>El artista se ha borrado correctamente</h3>
 @endif
- <div class="container">
-        <div class="breadcrumb navbar-form">
-            <form method="GET" action="{{ action('ArtistController@en6') }}">
-                <button type="summit" class="btn btn-info">Mostrar de 6 en 6</button>
-            </form>
-            <form method="GET" action="{{ action('ArtistController@ordenar') }}">
-                <button type="summit" class="btn btn-info">Ordenar por nombre</button>
-            </form>
-            <form method="get" action="{{ action('ArtistController@busqueda') }}">
-                <div class="form-group">
-                    <input type="text" class="form-control" name="buscado">
-                    <button type="summit" class="btn btn-warning">Buscar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-<h1 style="background-color:rgb(90,90,90);color:white; text-align: center">Artistas</h1>   
+<div>
+<h1 style="background-color:rgb(0,0,0);color:white; font-weight:bold; text-align:center">Artistas</h1>
+</div>
 <ul>
     @forelse($artists as $artist)
         <a href="{{action('ArtistController@Details', $artist->permalink)}}">
             <img class = "lista-artistas-prinicpal" src="{{ asset('images/artistas/' . trim($artist->permalink) . '/' . 'profile.jpg') }}">
         </a>
+        
     @empty
         <h2>No hay artistas en la BD</h2>
         
@@ -36,8 +23,24 @@
     {{ $artists->links() }}
 </ul style="padding-left: 62px;">
 <p>
-    <input type="button" onclick="location.href='{{action('ArtistController@FormNew')}}';" value="Nuevo artista"/>
-    <input type="button" onclick="location.href='/';" value="Inicio"/>
+    <div class="navbar-form">
+        <form method="get" action="{{ action('ArtistController@busqueda') }}">
+        <input type="button" class="boton" onclick="location.href='{{ action('ArtistController@ordenar') }}';" value="Ordenar por nombre"/>
+        <input type="button" class="boton" onclick="location.href='{{ action('ArtistController@en6') }}';" value="Mostrar de 6 en 6"/>
+                    <input type="text" class="form-control" name="buscado" >
+                    <button type="summit" class="btn btn-warning">Buscar</button>
+
+
+     </form>
+           
+    </div>
+    <br>
+    <div class="navbar-form">
+        <input type="button" class="boton2" onclick="location.href='{{action('ArtistController@FormNew')}}';" value="Nuevo artista"/>
+        <input type="button" class="boton3" onclick="location.href='/';" value="Inicio"/>
+        <br><br><br>
+    </div>
 </p>
 
+</div>
 @endsection
